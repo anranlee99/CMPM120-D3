b538e2b5b302bbfdbe9249fd4f8ea52d5a33c38e
An-Ran Lee <alee208@ucsc.edu>
2023-05-11 23:10:56 -0700

    so close yet so far

--- game.js
+++ game.js
@@ -3,99 +3,63 @@ class Example extends Phaser.Scene {
         super();
         this.delete = []
     }
-    
+
     drawTrajectory() {
 
         this.graphics.clear();
 
 
-        const p0 = new Phaser.Math.Vector2(this.ball.position.x, this.ball.position.y);
-        const length = 50;
-        const A = Phaser.Math.Distance.Between(this.ball.position.x,this.ball.position.y, this.dot.position.x, this.dot.position.y) - length 
-        const k = 0.03
-        const v = Math.sqrt(k * A * A)
-        let theta = Phaser.Math.Angle.Between(this.ball.position.x, this.ball.position.y, this.dot.position.x, this.dot.position.y)
-        const x1 = v * v * 2* Math.sin(theta) * Math.cos(theta) 
+        //y = (x-x0)tan(theta) - (g(x-x0)^2) / (2*(v*cos(theta))^2) + y0
+        let g = 1;
+        let x0 = this.ball.position.x;
+        let y0 = this.ball.position.y;
+        let k = 0.03;
+        let disp = Phaser.Math.Distance.Between(this.ball.position.x, this.ball.position.y, this.dot.position.x, this.dot.position.y) - 50
+        let v = Math.sqrt(2 * k * Math.pow(disp, 2))
+        let theta = (Math.PI * 2) - Phaser.Math.Angle.Between(this.ball.position.x, this.ball.position.y, this.dot.position.x, this.dot.position.y)
 
-        let y1 = x1 * Math.tan(theta) - ((x1 * x1) / (2 * v * v * Math.cos(theta) * Math.cos(theta)))
-        const p1 = new Phaser.Math.Vector2(this.ball.position.x - x1, y1)
-        const x2 = 2 * A * Math.cos(theta) * Math.cos(theta) * Math.tan(theta)
-        console.log(x2)
-        const p2 = new Phaser.Math.Vector2(this.ball.position.x - x2, this.ball.position.y)
-
-        const curve = new Phaser.Curves.QuadraticBezier(p0, p1, p2);
-        
-        this.graphics.lineStyle(1, 0xffffff, 1);
-        
-        curve.draw(this.graphics, 64);
-        this.graphics.fillStyle(0xff0000, 1);
-        this.graphics.fillCircle(p0.x, p0.y, 8);
-        this.graphics.fillStyle(0x00ff00, 1);
-        this.graphics.fillCircle(p1.x, p1.y, 8);
-        this.graphics.fillStyle(0x0000ff, 1);
-        this.graphics.fillCircle(p2.x, p2.y, 8);
-
-
-        /*
-        GIVEN:
-        p0 is this.ball.position
-        mass = 1; dont care
-        g = 1; dont care
-        v = sqrt((k*A^2) / mass)
-        k = 0.03
-        A = Phaser.Math.Distance.between(this.ball.position.x,this.ball.position.y, this.dot.position.x, this.dot.position.y) - length
-        v = sqrt((0.03 * A^2))
-        y = x tan(theta) - (g x^2) / (2 v^2 cos^2(theta))
-
-        
-        y = xtan(theta) - (g x^2) / (2 (0.03 * A^2)^2 cos^2(theta))
-        y = xtan(theta) - (g x^2) / (2 (0.0009 * A^4) cos^2(theta))
-        theta = Phaser.Math.Angle.between(this.ball.position.x, this.ball.position.y, this.dot.position.x, this.dot.position.y)
-
-        solving for p1 aka the apex:
-        dy/dx = tan(theta) - (g x) / (v^2 cos^2(theta)) = 0
-        dy/dx = tan(theta) - (x) / (0.0009 * A^4 cos^2(theta)) = 0
-        x1 = (v^2 sin(2theta)) 
-        x1 = (0.03 * A^2 * sin(2theta))
-        y1 = xtan(theta) - (g x^2) / (2 (0.0009 * A^4) cos^2(theta))
-        which is p1
-        
-        A = Phaser.Math.Distance.between(this.ball.position.x,this.ball.position.y, this.dot.position.x, this.dot.position.y) - length 
-        p2 is where 0 = x tan(theta) - (g x^2) / (2 v^2 cos^2(theta))
-        0 = 2v^2cos^2(theta) x tan(theta) - gx^2
-        0 = 2A*cos^2(theta)*x*tan(theta) - x^2
-        x ^2 = 2A*cos^2(theta)*x*tan(theta)
-        x2 = 2A*cos^2(theta)*tan(theta)
-
-        qed?
-        p0 = (this.ball.position.x, this.ball.position.y)
-        p1 = (x1, y1)
-        p2 = (x2, this.ball.position.y)
-
-
-
-
-
-        */
-
-        const points = curve.getSpacedPoints(20);
-
-        this.graphics.fillStyle(0xff0000, 1);
-        //  Draw the points
-        this.graphics.fillStyle(0xff0000, 1);
-
-        for (let i = 0; i < points.length; i++) {
-            this.graphics.fillCircle(points[i].x, points[i].y, 4);
+        function y(x) {
+            let res = (x - x0) * Math.tan(theta) - (g * Math.pow(x - x0, 2)) / (2 * Math.pow(v * Math.cos(theta), 2)) + y0;
+            return res;
         }
-    }
-    
 
-    
+        this.graphics.fillStyle(0xff0000, 1);
+
+        for (let i = x0; i < this.w; i += this.w / 10) {
+            this.graphics.fillCircle(i, this.h - y(i), 5);
+        }
+        // this.graphics.fillStyle(0xff0000, 1);
+        // for (let i = 0; i < this.w; i += this.w / 10) {
+        //     this.graphics.fillCircle(this.ball.position.x + i, y(this.ball.position.x + i), 5);
+        // }
+
+
+
+
+
+        // const curve = new Phaser.Curves.QuadraticBezier(p0, p1, p2);
+
+        // curve.draw(this.graphics, 64);
+
+        // const points = curve.getSpacedPoints(20);
+
+        // this.graphics.fillStyle(0xff0000, 1);
+        // //  Draw the points
+        // this.graphics.fillStyle(0xff0000, 1);
+
+        // for (let i = 0; i < points.length; i++) {
+        //     this.graphics.fillCircle(points[i].x, points[i].y, 4);
+        // }
+    }
+
+
+
     create() {
         this.graphics = this.add.graphics();
         this.matter.world.setBounds();
         this.w = this.game.config.width
         this.h = this.game.config.height
+
 
         const platform = this.matter.add.rectangle(this.w * 0.25, this.h * 0.75, this.w / 2, this.h / 2, { isStatic: true })
 
@@ -107,7 +71,7 @@ class Example extends Phaser.Scene {
         };
         this.ball = this.matter.add.circle(this.w * 0.25, this.h * 0.25 + 50, 32)
         this.spring = this.matter.add.spring(this.ball, this.dot, 50, 0.03);
-        console.log(this.spring)
+        // console.log(this.spring)
         /*
         v = sqrt(2 * k * d / m)
 
@@ -137,7 +101,7 @@ class Example extends Phaser.Scene {
         this.maxV = 0;
         this.fakeV = 0;
         this.predict = true
-        console.log(this.matter.world)
+        // console.log(this.matter.world)
     }
     update() {
         const damping = 0.03; // Damping coefficient
@@ -241,3 +205,5 @@ const config = {
 };
 
 let game = new Phaser.Game(config);
+// Set the origin to the bottom-left corner of the screen
+console.log(game.scale)
