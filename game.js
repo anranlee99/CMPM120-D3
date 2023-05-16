f66c5b7c85b17ce7b563dd6008f8601b21c1f6b5
An-Ran Lee <alee208@ucsc.edu>
2023-05-15 22:17:34 -0700

    safety commit cuz i dont remember what i did

--- game.js
+++ game.js
@@ -6,24 +6,52 @@ class Example extends Phaser.Scene {
     drawTrajectory() {
 
         this.graphics.clear();
-        //credit: Adam Smith
+        //credit: adam smith 
+        let hWorld = new Phaser.Physics.Matter.World(this, this.matter.config);
         let hFactory = new Phaser.Physics.Matter.Factory(hWorld);
         let hDot = hFactory.circle(this.w * 0.25, this.h * 0.25, 0, { isStatic: true })
-        let hBall = hFactory.circle(this.ball.position.x, this.ball.position.y , 32);
+        let hBall = hFactory.circle(this.ball.position.x, this.ball.position.y, 32);
         let hSpring = hFactory.spring(hBall, hDot, this.spring.length, this.spring.stiffness);
-        const step = 1000/60;
-        hWorld.update(0,step);
+        const step = 1000 / 60;
+        hWorld.update(0, step);
         hWorld.removeConstraint(hSpring);
 
-        for(let t = 0; t < 1000; t += step) {
-            let {x,y} = hBall.position;
-            this.graphics.fillCircle(x, y, 3);
-            hWorld.update(t,step);
+        if (Math.abs(hBall.position.y - this.ball.position.y) > 1) {
+            for (let t = 0; t < 1000; t += step) {
+                let { x, y } = hBall.position;
+                this.graphics.fillCircle(x, y, 3);
+                hWorld.update(t, step);
+            }
         }
 
     }
 
-
+    checkGameOver() {
+        let blocks_highest = this.blocks.reduce((acc, cur) => {
+            return acc < cur.position.y ? acc : cur.position.y
+        }, this.h)
+        
+    }
+    checkFired(){
+        //release the spring if the ball is far enough away
+        let disp = Phaser.Math.Distance.Between(this.ball.position.x, this.ball.position.y, this.dot.position.x, this.dot.position.y);
+        if (disp > this.ball.circleRadius && !this.input.activePointer.isDown) {
+            this.matter.world.removeConstraint(this.spring)
+            this.predict = false
+            this.graphics.clear()
+            //remove the mouse spring
+            const d = this.matter.world.localWorld.constraints.filter((c) => {
+                return c.label === "Pointer Constraint"
+            })
+            d.forEach((constraint) => {
+                this.matter.world.removeConstraint(constraint)
+            })
+            // this.matter.world.remove(this.matter.world.constraints[0])
+        }
+        if (this.predict) {
+            this.drawTrajectory()
+        }
+    }
 
     create() {
         this.graphics = this.add.graphics();
@@ -39,42 +67,51 @@ class Example extends Phaser.Scene {
             mask: 0x0000
         };
         //the ball and spring
-        this.ball = this.matter.add.circle(this.w * 0.25, this.h * 0.25 , 32)
+        const canDrag = this.matter.world.nextGroup();
+        this.ball = this.matter.add.circle(this.w * 0.25, this.h * 0.25, 32, { collisionFilter: { group: canDrag } });
         this.spring = this.matter.add.spring(this.ball, this.dot, 0, 0.03);
         this.spring.render.visible = false;
-        // console.log(this.spring.length, this.ball.mass, this.matter.world.localWorld)
-        console.log(this.ball)
-
-        const blocks = []
+        this.matter.add.mouseSpring
+        this.matter.add.mouseSpring({ collisionFilter: { group: canDrag } });
+        this.blocks = []
         for (let i = 0; i < 10; i++) {
             let b = this.matter.add.rectangle(this.w * 0.75, this.h * i / 10, 100, this.h * 0.09)
-            blocks.push(b)
+            b.setSleepStartEvent(false)
+            this.blocks.push(b)
         }
-        this.matter.add.mouseSpring();
+        console.log(this.matter.world.localWorld)
         this.predict = true
+        this.add.text(this.w, 0, "restart", {
+            fontSize: '32px',
+        }).setOrigin(1,0).setPadding(10).setInteractive().on('pointerdown', () => {
+            this.scene.restart()
+        })
+        this.time.addEvent({
+            delay: 1000,
+            callback: () => {
+                console.log(this.blocks[0])
+            },
+            repeat: -1
+        }) 
     }
     update() {
+        
+        this.checkGameOver()
         if (this.predict) {
-            this.drawTrajectory()
+            this.checkFired()
         }
-        //release the spring if the ball is far enough away
-        let disp = Phaser.Math.Distance.Between(this.ball.position.x, this.ball.position.y, this.dot.position.x, this.dot.position.y);
-        if (disp > this.ball.circleRadius && !this.input.activePointer.isDown) {
-            this.matter.world.removeConstraint(this.spring)
-            this.predict = false
-            this.graphics.clear()
-
-        }
+        
+        
     }
 }
 
 const config = {
     scale: {
-        mode: Phaser.Scale.FIT,
+        // mode: Phaser.Scale.FIT,
         autoCenter: Phaser.Scale.CENTER_BOTH,
-        width: 1920 ,
+        width: 1920,
         height: 1080
-    }, 
+    },
     backgroundColor: '#000000',
     resolution: 4,
     parent: 'phaser-example',
